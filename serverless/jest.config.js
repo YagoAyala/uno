682f5c599f79299b5ea11c7aa0c62bd5b18e1dbd
YagoAyala <yago.ayala@outlook.com>
2025-05-05 23:04:24 -0300

    chore: changed threshold from jest

--- serverless/jest.config.js
+++ serverless/jest.config.js
@@ -7,6 +7,6 @@ module.exports = {
     '!src/db.js'
   ],
   coverageThreshold: {
-    global: { branches: 10, functions: 25, lines: 50, statements: 25 }
+    global: { branches: 10, functions: 25, lines: 25, statements: 25 }
   }
 };
